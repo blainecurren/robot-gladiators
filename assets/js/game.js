15eb266b9f8d9d7ef1e066bebc9c88f2ff15a552
Blaine Curren <blaine.curren@gmail.com>
2022-10-30 05:32:18 -0500

    updated fight vars

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,14 +2,45 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 
-console.log(plaerName, playerHealth, playerAttack);
+console.log(playerName, playerHealth, playerAttack);
 
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function () {
+  // Alert players that they are starting the round
   window.alert("welcome to Robot Gladiators!");
+
+  // Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  enemyHealth = enemyHealth - playerAttack;
+
+  // Log a resulting message to the console
+  console.log(
+    playerName +
+      " attacked " +
+      enemyName +
+      ". " +
+      enemyName +
+      " now has " +
+      enemyHealth +
+      " health remaining"
+  );
+
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
+  playerHealth = playerHealth - enemyAttack;
+
+  // Log a resulting message to the console
+  console.log(
+    enemyName +
+      " attacked " +
+      playerName +
+      ". " +
+      playerName +
+      " now has " +
+      playerHealth +
+      " health remaining."
+  );
 };
 
 fight();
