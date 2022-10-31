27b466ffc073dade2555cb0dc3033ef0ef334ebf
Blaine Curren <blaine.curren@gmail.com>
2022-10-31 13:34:48 -0500

    updated enemyNames array

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,17 +3,17 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerHealth, playerAttack);
-
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 for (var i = 0; i < enemyNames.length; i++) {
   console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + " is at " + i + " index");
 }
 
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+var fight = function (enemyName) {
   // Alert players that they are starting the round
   window.alert("welcome to Robot Gladiators!");
 
